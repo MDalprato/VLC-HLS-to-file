a55d42d7706367a5c5db42d6ac24c6c5829e28f5
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2020-04-16 19:29:49 +0200

    Create file.cmd
    Added new file

---
+++ file.cmd
@@ -0,0 +1,22 @@
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "https://2uscreativem3-vh.akamaihd.net/i/archive_e/Archive/raidue/1241333.mp4/index_0_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e0.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "https://2uscreativem3-vh.akamaihd.net/i/archive_e/Archive/raidue/1240757.mp4/index_0_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e1.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://creativemedia4.rai.it/archive_e/Archive/raidue/1240757.mp4" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e2.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://uscreativemedia6-vh.akamaihd.net/i/podcastcdn/raidue/Pechino_Express/pechinoexpress_puntate_2012/1100359_,400,600,800,1200,1500,1800,.mp4.csmil/index_5_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128, vlc://quitchannels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e3.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://uscreativemedia6-vh.akamaihd.net/i/podcastcdn/raidue/Pechino_Express/pechinoexpress_puntate_2012/1105827_,400,600,800,1200,1500,1800,.mp4.csmil/index_5_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2, vlc://quitsamplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e4.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://uscreativemedia6-vh.akamaihd.net/i/podcastcdn/raidue/Pechino_Express/pechinoexpress_puntate_2012/1116947_,400,600,800,1200,1500,1800,.mp4.csmil/index_5_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e5.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://uscreativemedia6-vh.akamaihd.net/i/podcastcdn/raidue/Pechino_Express/pechinoexpress_puntate_2012/1127832_,400,600,800,1200,1500,1800,.mp4.csmil/index_5_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e6.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://uscreativemedia6-vh.akamaihd.net/i/podcastcdn/raidue/Pechino_Express/pechinoexpress_puntate_2012/1138907_,400,600,800,1200,1500,1800,.mp4.csmil/index_5_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e7.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://uscreativemedia6-vh.akamaihd.net/i/podcastcdn/raidue/Pechino_Express/pechinoexpress_puntate_2012/1149964_,400,600,800,1200,1500,1800,.mp4.csmil/index_5_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e8.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://uscreativemedia6-vh.akamaihd.net/i/podcastcdn/raidue/Pechino_Express/pechinoexpress_puntate_2012/1160291_,400,600,800,1200,1500,1800,.mp4.csmil/index_5_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e9.mp4'} vlc://quit
+
+"C:\Program Files\VideoLAN\VLC\vlc.exe" "http://uscreativemedia6-vh.akamaihd.net/i/podcastcdn/raidue/Pechino_Express/pechinoexpress_puntate_2012/1173251_,400,600,800,1200,1500,1800,.mp4.csmil/index_5_av.m3u8" --sout=#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=44100}:std{access=file{no-overwrite},mux=mp4,dst='D:/pech/s1e10.mp4'} vlc://quitac
